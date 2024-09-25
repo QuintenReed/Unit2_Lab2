bbe0f1d78d2c4c109007b23d52afe557d517585f
Quinten Reed <qreed26@cpatech.org>
2024-09-25 14:35:25 +0000

    my notes

--- ArrayClass.py
+++ ArrayClass.py
@@ -46,7 +46,7 @@ class DynamicArray():
   def __resize(self):
     self.__capacity *= 2
 
-    new_list = __make_array(self.__capacity)
+    new_list = self.__make_array(self.__capacity)
 
     for i in range(self.__n):
       new_list[i] = self.__A[i]
